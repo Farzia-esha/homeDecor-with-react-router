f8e3144b86e4de134793ac9cce523c7be7a397fb
Farzia-esha <farziakhandokar24@gmail.com>
2025-10-06 18:06:04 +0600

    HomeDecor

---
+++ src/Pages/ProductDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetails = () => {
+    return (
+        <div>
+            details of products
+        </div>
+    );
+};
+
+export default ProductDetails;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router";
+import Home from "../Pages/Home";
+import Products from "../Pages/Products";
+import MainLayout from "../Layouts/MainLayout";
+import Wishlist from "../Pages/Wishlist";
+import ProductDetails from "../Pages/ProductDetails";
+import ErrorPage from "../Pages/ErrorPage";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout />,
+        errorElement:<ErrorPage />,
+        children:[
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/products',
+                element: <Products />
+            },
+            {
+                path: '/wishlist',
+                element: <Wishlist />
+            },
+            {
+                path: '/product/:id',
+                element: <ProductDetails />
+            },
+
+        ]
+    },
+// {
+//     path:'*',
+//     element:<ErrorPage />,
+// }
+
+])
+
+export default router
